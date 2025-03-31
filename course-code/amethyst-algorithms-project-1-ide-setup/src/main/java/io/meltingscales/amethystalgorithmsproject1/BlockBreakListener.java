9a3e127354d4b3805ac287f2cf66658ff53fee5f
meltingscales <HenryFBP@gmail.com>
2025-03-31 16:46:52 -0500

    improve name

--- course-code/amethyst-algorithms-project-1-ide-setup/src/main/java/io/meltingscales/amethystalgorithmsproject1/BlockBreakListener.java
+++ course-code/amethyst-algorithms-project-1-ide-setup/src/main/java/io/meltingscales/amethystalgorithmsproject1/BlockBreakListener.java
@@ -13,17 +13,17 @@ public class BlockBreakListener {
   @SubscribeEvent(receiveCanceled = true)
   public static void onBlockBreak(BlockEvent.BreakEvent event) {
     // Get the block state and position
-    BlockState state = event.getState();
-    BlockPos pos = event.getPos();
+    BlockState blockState = event.getState();
+    BlockPos blockPos = event.getPos();
     // Check if the block being broken is the one you're interested in
-    if (state.getBlock() == Blocks.STONE) {
+    if (blockState.getBlock() == Blocks.STONE) {
       // Do something when a stone block is broken
-      System.out.println("Stone block broken at: " + pos);
+      System.out.println("Stone block broken at: " + blockPos);
     }
 
-    if (state.getBlock() == Blocks.DIRT) {
+    if (blockState.getBlock() == Blocks.DIRT) {
 
-      System.out.println("Dirt block broken at: " + pos);
+      System.out.println("Dirt block broken at: " + blockPos);
       System.out.println("Going to drop a gold ingot");
 
       // drop a gold ingot as a test
@@ -32,11 +32,25 @@ public class BlockBreakListener {
           .addFreshEntity(
               new net.minecraft.world.entity.item.ItemEntity(
                   (Level) event.getLevel(),
-                  pos.getX() + 0.5,
-                  pos.getY() + 0.5,
-                  pos.getZ() + 0.5,
+                  blockPos.getX() + 0.5,
+                  blockPos.getY() + 0.5,
+                  blockPos.getZ() + 0.5,
                   new net.minecraft.world.item.ItemStack(
                       net.minecraft.world.item.Items.GOLD_INGOT)));
+    }
+    // also generate 10 smoke particles for any block break
+
+    for (int i = 0; i < 10; i++) {
+      event
+          .getLevel()
+          .addParticle(
+              net.minecraft.core.particles.ParticleTypes.SMOKE,
+              blockPos.getX(),
+              blockPos.getY(),
+              blockPos.getZ(),
+              0,
+              0,
+              0);
     }
   }
 }
